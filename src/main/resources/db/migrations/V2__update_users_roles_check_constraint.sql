f78fc5b05283402719b104f49ca824b687ca79c7
XzibiT-Collins <huvisoncollins@gmail.com>
2026-04-27 07:57:48 +0000

    chore(db-migrations): fix migrations by creating a new V1 with all tables

--- src/main/resources/db/migrations/V2__update_users_roles_check_constraint.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE users
-    DROP CONSTRAINT IF EXISTS users_roles_check;
-
-ALTER TABLE users
-    ADD CONSTRAINT users_roles_check
-        CHECK (roles IN ('ADMIN', 'CUSTOMER', 'FRONT_DESK'));
